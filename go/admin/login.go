d5073abedac20a39ce809515a24b2b65dc2d76f3
fizzzmath <stasha0601@gmail.com>
2025-05-26 14:25:59 +0300

    Add files via upload

--- go/admin/login.go
+++ go/admin/login.go
@@ -6,11 +6,34 @@ import (
 	"net/http"
 )
 
+type (
+	Admin struct {
+		Login string
+		Password string
+	}
+)
+
+func valid(admin Admin) bool {
+	return false
+}
+
 func loginHandler(w http.ResponseWriter, r *http.Request) {
 	tmpl, err := template.ParseFiles("admin/login.html")
 
 	if err != nil {
 		fmt.Fprintf(w, "Template error: %v", err)
+	}
+
+	if r.Method == http.MethodPost {
+		admin := Admin {
+			Login: r.FormValue("login"),
+			Password: r.FormValue("password"),
+		}
+
+		if !valid(admin) {
+			tmpl.Execute(w, "Неверные логин или пароль")
+			return
+		}
 	}
 
 	tmpl.Execute(w, nil)
