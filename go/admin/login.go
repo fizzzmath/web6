671982ef31fb19b2af2b7febbaeb1b12c17cc136
fizzzmath <stasha0601@gmail.com>
2025-05-26 14:45:12 +0300

    Add files via upload

--- go/admin/login.go
+++ go/admin/login.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"html/template"
 	"net/http"
@@ -13,8 +14,41 @@ type (
 	}
 )
 
-func valid(admin Admin) bool {
-	return false
+func valid(admin Admin) (bool, error) {
+	db, err := sql.Open("mysql", "u68867:6788851@/u68867")
+
+	if err != nil {
+		return false, err
+	}
+
+	defer db.Close()
+
+	login, password := "", ""
+
+	sel, err := db.Query(`
+		SELECT Login, Password 
+		FROM ADMIN
+	`)
+
+	if err != nil {
+		return false, err
+	}
+
+	defer sel.Close();
+
+	for sel.Next() {
+		err := sel.Scan(&login, &password)
+
+		if err != nil {
+			return false, err
+		}
+	}
+
+	if login != admin.Login || fmt.Sprintf("%x", password) != admin.Password {
+		return false, nil
+	}
+
+	return true, nil
 }
 
 func loginHandler(w http.ResponseWriter, r *http.Request) {
@@ -30,7 +64,14 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 			Password: r.FormValue("password"),
 		}
 
-		if !valid(admin) {
+		ok, err := valid(admin)
+
+		if err != nil {
+			fmt.Fprintf(w, "MySQL error: %v", err)
+			return
+		}
+
+		if !ok {
 			tmpl.Execute(w, "Неверные логин или пароль")
 			return
 		}
